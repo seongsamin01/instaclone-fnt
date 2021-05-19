241ee8c3633ecc9e104433e5e254641282fbee87
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-05-19 16:16:02 +0900

    Router 사용법 2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./screens/Home";
+import Login from "./screens/Login";
+import NotFound from "./screens/NotFound";
 
 function App() {
+  const isLoggedIn = true;
   return (
-    <div>
-      <Router>
-        <Switch>
-          <Route path="/" exact>
-            <h1>Home</h1>
-          </Route>
-          <Route path="/potato">
-            <h1>Potato</h1>
-          </Route>
-          <Route path="/banana">
-            <h1>Banana</h1>
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          {isLoggedIn ? <Home /> : <Login />}
+        </Route>
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
