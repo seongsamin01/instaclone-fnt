ef09541650c41bdd639eecb27bdc60326f17a97e
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-05-19 18:02:24 +0900

    Styled Components 3 _GlobalStyles

--- src/App.js
+++ src/App.js
@@ -5,32 +5,24 @@ import { darkModeVar, isLoggedInVar } from "./apollo";
 import Home from "./screens/Home";
 import Login from "./screens/Login";
 import NotFound from "./screens/NotFound";
-
-const lightTheme = {
-    fontColor: "#2c2c2c",
-    bgColor: "lightgray",
-};
-
-const darkTheme = {
-    fontColor: "lightgray",
-    bgColor: "#2c2c2c",
-};
+import { darkTheme, GlobalStyles, lightTheme } from "./styles";
 
 function App() {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
   const darkMode = useReactiveVar(darkModeVar);
   return (
     <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-    <Router>
-      <Switch>
-        <Route path="/" exact>
-          {isLoggedIn ? <Home /> : <Login />}
-        </Route>
-        <Route>
-          <NotFound />
-        </Route>
-      </Switch>
-    </Router>
+      <GlobalStyles />
+        <Router>
+          <Switch>
+            <Route path="/" exact>
+              {isLoggedIn ? <Home /> : <Login />}
+            </Route>
+            <Route>
+              <NotFound />
+            </Route>
+          </Switch>
+        </Router>
     </ThemeProvider>
   );
 }
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -6,9 +6,7 @@ const Title = styled.h1`
     color: ${(props) => props.theme.fontColor};
 `;
 
-const Container = styled.div`
-    background-color: ${(props) => props.theme.bgColor};
-`;
+const Container = styled.div``;
 
 function Login() {
     return (
