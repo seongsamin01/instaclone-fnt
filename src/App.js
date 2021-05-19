529cfc1c3ea9e1f411bd680d28581a77a2743857
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-05-19 15:52:39 +0900

    Router 사용법 1(HashRouter & BrowserRouter)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <h1>Home</h1>
+          </Route>
+          <Route path="/potato">
+            <h1>Potato</h1>
+          </Route>
+          <Route path="/banana">
+            <h1>Banana</h1>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
