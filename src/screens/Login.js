ee01207f19b65ebcfd0c2098df0694db53e3d70a
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-05-20 15:07:47 +0900

    React Hook Form 1

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,5 +1,6 @@
 import { faFacebookSquare, faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useForm } from "react-hook-form";
 import styled from "styled-components";
 import AuthLayout from "../components/auth/AuthLayout";
 import BottomBox from "../components/auth/BottomBox";
@@ -19,7 +20,13 @@ const FacebookLogin = styled.div`
 `;
 
 function Login() {
-
+    const { register, handleSubmit } = useForm();
+    const onSubmitValid = (data) => {
+        console.log(data);
+    };
+    const onSubmitInvalid = (data) => {
+        console.log(data, "invalid");
+    };
     return (
             <AuthLayout>
                 <PageTitle title="Login"/>
@@ -27,9 +34,24 @@ function Login() {
                     <div>
                         <FontAwesomeIcon icon={faInstagram} size="3x" />
                     </div>
-                    <form>
-                        <Input type="text" placeholder="Username" />
-                        <Input type="password" placeholder="Password"/>
+                    <form onSubmit={handleSubmit(onSubmitValid, onSubmitInvalid)}>
+                        <Input 
+                            ref={register({
+                                required: "Username is required.",
+                                minLength: 5,
+                            })} 
+                            name="username"  
+                            type="text" 
+                            placeholder="Username" 
+                        />
+                        <Input 
+                            ref={register({
+                                required: "Password is required.",
+                            })} 
+                            name="password" 
+                            type="password" 
+                            placeholder="Password"
+                        />
                         <Button type="submit" value="Log in" />
                     </form>
                     <Seperator />
