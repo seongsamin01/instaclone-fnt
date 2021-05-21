75a1c27d8d0bdffd61a39f9e4c30790af70e555c
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-05-21 13:26:50 +0900

    Dark Mode

--- src/apollo.js
+++ src/apollo.js
@@ -4,7 +4,8 @@ import {
     makeVar,
  } from "@apollo/client";
 
-const TOKEN = "token";
+const TOKEN = "TOKEN";
+const DARK_MODE = "DARK_MODE";
 
 export const isLoggedInVar = makeVar(Boolean(localStorage.getItem(TOKEN)));
 
@@ -18,7 +19,17 @@ export const logUserOut = () => {
     window.location.reload();
 };
 
-export const darkModeVar = makeVar(false);
+export const darkModeVar = makeVar(Boolean(localStorage.getItem(DARK_MODE)));
+
+export const enableDarkMode = () => {
+    localStorage.setItem(DARK_MODE, "enabled");
+    darkModeVar(true)
+}
+
+export const disableDarkMode = () => {
+    localStorage.removeItem(DARK_MODE, "enabled");
+    darkModeVar(false)
+}
 
 export const client = new ApolloClient({
     uri: "http://localhost:4000/graphql",
