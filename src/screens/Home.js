892e51cc8ac226d7905a533215bbfc91c40504c4
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-05-21 18:24:45 +0900

    #11 isLiked (좋아요하트채우기)

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -7,7 +7,7 @@ import {
   } from "@fortawesome/free-regular-svg-icons";
   import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styled from "styled-components";
-import { logUserOut } from "../apollo";
+import { faHeart as SolidHeart } from "@fortawesome/free-solid-svg-icons";
 import Avatar from "../components/Avatar";
 import { FatText } from "../components/shared";
 
@@ -25,6 +25,7 @@ const FEED_QUERY = gql`
       comments
       createdAt
       isMine
+      isLiked
     }
   }
 `;
@@ -32,13 +33,15 @@ const FEED_QUERY = gql`
 const PhotoContainer = styled.div`
   background-color: white;
   border: 1px solid ${(props) => props.theme.borderColor};
-  margin-bottom: 20px;
+  margin-bottom: 60px;
   max-width: 615px;
+  border-radius: 4px;
 `;
 const PhotoHeader = styled.div`
   padding: 15px;
   display: flex;
   align-items: center;
+  border-bottom: 1px solid rgb(239, 239, 239);
 `;
 
 const Username = styled(FatText)`
@@ -47,10 +50,11 @@ const Username = styled(FatText)`
 
 const PhotoFile = styled.img`
   min-width: 100%;
+  max-width: 100%;
 `;
 
 const PhotoData = styled.div`
-  padding: 15px;
+  padding: 12px 15px;
 `;
 
 const PhotoActions = styled.div`
@@ -60,6 +64,9 @@ const PhotoActions = styled.div`
   div {
     display: flex;
     align-items: center;
+  }
+  svg {
+    font-size: 20px;
   }
 `;
 
@@ -87,17 +94,20 @@ function Home() {
                 <PhotoActions>
                 <div>
                     <PhotoAction>
-                    <FontAwesomeIcon size={"2x"} icon={faHeart} />
+                    <FontAwesomeIcon
+                        style={{ color: photo.isLiked ? "tomato" : "inherit" }}
+                        icon={photo.isLiked ? SolidHeart : faHeart}
+                    />
                     </PhotoAction>
                     <PhotoAction>
-                    <FontAwesomeIcon size={"2x"} icon={faComment} />
+                    <FontAwesomeIcon icon={faComment} />
                     </PhotoAction>
                     <PhotoAction>
-                    <FontAwesomeIcon size={"2x"} icon={faPaperPlane} />
+                    <FontAwesomeIcon icon={faPaperPlane} />
                     </PhotoAction>
                 </div>
                 <div>
-                    <FontAwesomeIcon size={"2x"} icon={faBookmark} />
+                    <FontAwesomeIcon icon={faBookmark} />
                 </div>
                 </PhotoActions>
                 <Likes>
