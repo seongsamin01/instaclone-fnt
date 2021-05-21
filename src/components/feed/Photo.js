d5d47932a50aa90beb5b29cd85830b8eb8423ef7
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-05-21 19:10:37 +0900

    #11 read Fragment (좋아요완성/cache)

--- src/components/feed/Photo.js
+++ src/components/feed/Photo.js
@@ -79,19 +79,30 @@ function Photo({ id, user, file, isLiked, likes }) {
           },
         } = result;
         if (ok) {
-          cache.writeFragment({
-            id: `Photo:${id}`,
-            fragment: gql`
-              fragment BSName on Photo {
+            const fragmentId = `Photo:${id}`;
+            const fragment = gql`
+                fragment BSName on Photo {
                 isLiked
+                likes
+            }
+          `;
+            const result = cache.readFragment({
+                id: fragmentId,
+                fragment,
+            });
+            if ("isLiked" in result && "likes" in result) {
+                const { isLiked: cacheIsLiked, likes: cacheLikes } = result;
+                cache.writeFragment({
+                  id: fragmentId,
+                  fragment,
+                  data: {
+                    isLiked: !cacheIsLiked,
+                    likes: cacheIsLiked ? cacheLikes - 1 : cacheLikes + 1,
+                  },
+                });
               }
-            `,
-            data: {
-              isLiked: !isLiked,
-            },
-          });
-        }
-      };
+            }
+        };
   const [toggleLikeMutation] = useMutation(TOGGLE_LIKE_MUTATION, {
     variables: {
       id,
